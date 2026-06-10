643adb5ed54cfbbd76bf9abe2fb1b104b8bd1b36
Piyush Kumar <piyushca2006@gmail.com>
2026-06-10 14:39:39 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,43 @@
+let current = 0;
 
+const numbers = [2,7,11,15];
+
+function render() {
+
+  let html = "";
+
+  numbers.forEach((num, idx) => {
+
+    const step = steps[current];
+
+    if(idx === step.left)
+      html += `[L:${num}] `;
+
+    else if(idx === step.right)
+      html += `[R:${num}] `;
+
+    else
+      html += `${num} `;
+  });
+
+  document.getElementById("array").innerHTML = html;
+
+  document.getElementById("explanation").innerText =
+      steps[current].text;
+}
+
+function nextStep() {
+  if(current < steps.length - 1){
+    current++;
+    render();
+  }
+}
+
+function prevStep() {
+  if(current > 0){
+    current--;
+    render();
+  }
+}
+
+render();
