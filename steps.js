bbb5342f7d958f39dee72e32ba29316d4c31d64e
Piyush Kumar <piyushca2006@gmail.com>
2026-06-10 14:50:51 +0530

    Update steps.js

--- steps.js
+++ steps.js
@@ -1,1 +1,17 @@
-
+const steps = [
+  {
+    left: 0,
+    right: 3,
+    text: "2 + 15 = 17 > 9. Move right pointer."
+  },
+  {
+    left: 0,
+    right: 2,
+    text: "2 + 11 = 13 > 9. Move right pointer."
+  },
+  {
+    left: 0,
+    right: 1,
+    text: "2 + 7 = 9. Found answer."
+  }
+];
