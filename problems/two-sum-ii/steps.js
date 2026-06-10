798d5dbaf59f164f03b1bd254b3d54e97cfc6128
Piyush Kumar <piyushca2006@gmail.com>
2026-06-10 15:35:35 +0530

    Update steps.js

--- problems/two-sum-ii/steps.js
+++ problems/two-sum-ii/steps.js
@@ -1,17 +1,27 @@
 const steps = [
   {
     left: 0,
+    right: 5,
+    text: "2 + 15 = 17 > 12. Move R left."
+  },
+  {
+    left: 0,
+    right: 4,
+    text: "2 + 11 = 13 > 12. Move R left."
+  },
+  {
+    left: 0,
     right: 3,
-    text: "2 + 15 = 17 > 9. Move right pointer."
+    text: "2 + 8 = 10 < 12. Move L right."
   },
   {
-    left: 0,
-    right: 2,
-    text: "2 + 11 = 13 > 9. Move right pointer."
+    left: 1,
+    right: 3,
+    text: "3 + 8 = 11 < 12. Move L right."
   },
   {
-    left: 0,
-    right: 1,
-    text: "2 + 7 = 9. Found answer."
+    left: 2,
+    right: 3,
+    text: "4 + 8 = 12. Found answer."
   }
 ];
